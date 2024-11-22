7da9b3232517e257bdd25c27de87f42ade6a0237
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2024-11-22 10:25:05 +0100

    klar

---
+++ Näsan steg 3 övning 5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class progrem
+{
+
+    static int MinMetod(int r)
+    {
+        return (r*r)*3;        //vet ej hur jag ska skriva talet PI och decimaler
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MinMetod(5) + "m^2");
+        Console.ReadLine();
+    }
+}
